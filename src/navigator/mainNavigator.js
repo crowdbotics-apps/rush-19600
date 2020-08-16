74974f2f7b0eb4cb214a20bde5dafcaa3de66857
Crowdbotics <team@crowdbotics.com>
2020-08-16 19:33:17 +0000

    remove package blueprint-react-native-maps-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,7 +6,6 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import UserProfile90973Navigator from '../features/UserProfile90973/navigator';
-import Maps90954Navigator from '../features/Maps90954/navigator';
 import Settings90932Navigator from '../features/Settings90932/navigator';
 import Settings90917Navigator from '../features/Settings90917/navigator';
 import NotificationList90916Navigator from '../features/NotificationList90916/navigator';
@@ -20,7 +19,6 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
 UserProfile90973: { screen: UserProfile90973Navigator },
-Maps90954: { screen: Maps90954Navigator },
 Settings90932: { screen: Settings90932Navigator },
 Settings90917: { screen: Settings90917Navigator },
 NotificationList90916: { screen: NotificationList90916Navigator },
